7894e976668a9d18e26b4ab2c53fca40fd6413b7
Robin-TAB <32072027+robin-tab@users.noreply.github.com>
2019-03-18 15:46:37 +0000

    Update replace_with_redacted.sh

--- replace_with_redacted.sh
+++ replace_with_redacted.sh
@@ -1,5 +1,5 @@
 input_file=$1
-for uid in james	tom.whitbrook	brett	stephen	joe.wapshott	marc.hernandez	074	075	076	077	078	079	; do
+for uid in text-to-search-for	; do
 
   cat $input_file | sed -e "s!$uid!REDACTED!" > ${input_file}.tmp
   mv ${input_file}.tmp $input_file
